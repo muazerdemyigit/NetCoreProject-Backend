cd4cc964f82b994e1d24e44a48e21c50c531e86b
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-10-13 07:42:50 +0300

    Day 3 / Part 2

---
+++ Entities/Concrete/Order.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Concrete
+{
+    public class Order : IEntity
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipCity { get; set; }
+    }
+
+}
