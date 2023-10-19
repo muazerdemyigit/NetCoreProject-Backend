79ec91b821e27f27b19194c924500f9a77b7fd52
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-10-19 07:04:50 +0300

    Day 4 / Part 3

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -4,5 +4,7 @@
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
     }
 }
