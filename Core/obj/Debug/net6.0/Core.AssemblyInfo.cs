23a33ff0284053c26d986855382881862ce6a65f
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-11-22 19:01:17 +0300

    Day 8 / Part 4

--- Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c1b5542c44979d15c317bd118b4a226f33af94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
